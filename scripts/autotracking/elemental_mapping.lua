5589d5ac646782176d1356ef8824335dac9abd59
FyreDay <fyredaymc@gmail.com>
2025-10-27 22:23:15 -0500

    Fixed progressive rang

--- scripts/autotracking/elemental_mapping.lua
+++ scripts/autotracking/elemental_mapping.lua
@@ -1,8 +1,8 @@
 ELEMENTAL_MAPPING = {
 	[1] = {"secondrang"},
     [2] = {"swim"},
-    [3] = {"dive"},
-    [4] = {"aquarang"},
+    [3] = {"aquarang"},
+    [4] = {"dive"},
     [5] = {"flamerang"},
     [6] = {"frostyrang"},
     [7] = {"zappyrang"},
